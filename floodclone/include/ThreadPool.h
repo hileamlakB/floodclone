31436e986fdca6ab73bafea5c729a1dd51a28154
hileamla <hileamla@mit.edu>
2024-11-05 00:50:58 +0000

    Implement Test, completed send, receiver, and reconstruct

--- floodclone/include/ThreadPool.h
+++ floodclone/include/ThreadPool.h
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <functional>
+#include <atomic>
 
 class ThreadPool {
 public:
@@ -13,6 +14,7 @@ public:
     ~ThreadPool();
     void enqueue(std::function<void()> task);
     void join();
+    void wait(); // waits until task ueue is empty
 
 private:
     std::vector<std::thread> workers;
@@ -21,6 +23,9 @@ private:
     std::mutex queue_mutex;
     std::condition_variable condition;
     bool stop;
+
+    std::condition_variable idle_condition;
+    std::atomic<int> active_tasks{0};
 };
 
 
